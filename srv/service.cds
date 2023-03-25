b38456288f1752ab070df0a6e83ed2bf13177d3e
Helen Yang <15968822>
2023-03-25 23:02:45 +0000

    Added unapprove and post actions

--- srv/service.cds
+++ srv/service.cds
@@ -4,7 +4,9 @@ service LaasDeductionsService {
     entity Deductions as projection on my.Deductions actions {
         @( cds.odata.bindingparameter.name : '_it', Common.SideEffects : {TargetProperties: ['_it/status']} )
         action approve();
+        @( cds.odata.bindingparameter.name : '_it', Common.SideEffects : {TargetProperties: ['_it/status']} )
         action unapprove();
+        @( cds.odata.bindingparameter.name : '_it', Common.SideEffects : {TargetProperties: ['_it/status']} )
         action post();
     };
 }
--- srv/service.js
+++ srv/service.js
@@ -9,17 +9,13 @@ class LaasDeductionsService extends cds.ApplicationService {
             await cds.update(Deductions, req.params[0].ID).set({status: "APPROVED"});
         });
     
-    /*    this.on('approve', async (req) => {
-            console.log(req.params);
-            const id = req.params[0];
-            console.log(id);
-            const n = await UPDATE(Deductions).set({
-            status: "APPROVED",
-            approvedDate: new Date(),
-            approvedBy: req.user.id }).where({ID: id}); 
-            console.log(n);
-        })  */
+        this.on('unapprove', Deductions, async req => {
+            await cds.update(Deductions, req.params[0].ID).set({status: "UNAPPROVED"});
+        });
 
+        this.on('post', Deductions, async req => {
+            await cds.update(Deductions, req.params[0].ID).set({status: "POSTED"});
+        });
       
         // required
         await super.init()
